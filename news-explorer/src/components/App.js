29c37baeb78b57793896c2d2f156121f89704a2b
malhane carvalho <malhanec@gmail.com>
2024-11-25 19:49:00 +0000

    feat: creates font and image directories and adds their respective files

--- news-explorer/src/components/App.js
+++ news-explorer/src/components/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import background from '../images/georgia-de-lotz-unsplash.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={background} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
